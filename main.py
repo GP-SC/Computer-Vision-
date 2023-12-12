182b171e9425ae3a95f0e28963f4c528d69d8175
mohamedhazem24 <mhazemcs@gmail.com>
2023-12-12 21:21:46 +0200

    1st thing

--- main.py
+++ main.py
@@ -3,65 +3,53 @@ import cv2
 import os
 import glob
 from PIL import Image
+import matplotlib.pyplot as plt
+class Image:
+    def __init__(self,img,label:str):
+        self.img = img
+        self.label=label
+        self.pred=None
+    def SetPred(self,pred):
+        self.pred=pred
+def preprocessing(DataSet:list(Image)):
+    for image in DataSet:
+        img=image.img
 
-def preprocessing(img):
-    gray_image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    new_width, new_height = 300, 200
-    resized_image = cv2.resize(gray_image, (new_width, new_height))
-    normalized_image = resized_image / 255.0
-    cv2.imshow('Original Image', img)
-    cv2.imshow('Grayscale Image', gray_image)
-    cv2.imshow('Resized Image', resized_image)
-    cv2.imshow('Normalized Image', normalized_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        '''new_width, new_height = 300, 200
+        resized_image = cv2.resize(gray_image, (new_width, new_height))
+        normalized_image = resized_image / 255.0
+'''
+def Load_Dataset():
+    currpath="Data/Product Classification"
+    Classes=os.listdir(currpath)
+    Classes.sort( key=lambda x: int(x))
+    Classes=list(map(lambda x:currpath+"/"+x,Classes))
+    X_train,y_train,X_valid,y_valid=list()
+    trainset=list()
+    validset=list()
+    for Class in Classes:
+        trainpart=os.listdir(Class+"/"+"Train")
+        trainpart=list(map(lambda x:Class+"/"+"Train"+"/"+x,trainpart))
+        for file in trainpart:
+            img=cv2.imread(file,0)
+            label=Class.split("/")[-1]
+            trainset.append(Image(img,label))
+            X_train.append(img)
+            y_train.append(label)
+        if "Validation"  not in os.listdir(Class):continue
+        validpart=os.listdir(Class+"/"+"Validation")
+        validpart=list(map(lambda x:Class+"/"+"Validation"+"/"+x,validpart))
+        for file in validpart:
+            img=cv2.imread(file,0)
+            label=Class.split("/")[-1]
+            validset.append(Image(img,label))
+            X_valid.append(img)
+            y_valid.append(label)
+    return trainset,validset,X_train,y_train,X_valid,y_valid
 
-def readtrainimage(dataset_path):
- # You may need to install the Pillow library for image processing
 
-    # Specify the path to the root folder
-    root_path = dataset_path
+trainset,validset,X_train,y_train,X_valid,y_valid=Load_Dataset()
 
-    # Specify the main folders
-    data_folder = "Train"
-    classification_product_folder = "Product Classification"
-
-    # Get the list of product folders
-    product_folders = [folder for folder in os.listdir(os.path.join(root_path, classification_product_folder)) if
-                       os.path.isdir(os.path.join(root_path, classification_product_folder, folder))]
-
-    # Iterate through each product folder
-    for product_folder in product_folders:
-        product_path = os.path.join(root_path, classification_product_folder, product_folder)
-
-        # Check for the existence of the training folder
-        training_folder = os.path.join(product_path, data_folder, "training")
-
-        if os.path.exists(training_folder):
-            # Get a list of image files in the training folder
-            image_files = [os.path.join(training_folder, file) for file in os.listdir(training_folder) if
-                           file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
-
-            # Iterate through each image file in the training folder
-            for image_file in image_files:
-                # You can now perform operations on each image
-                # For example, open the image using Pillow
-                img = Image.open(image_file)
-
-                # Add your image processing or analysis code here
-
-                # Optionally, you can display the image
-                img.show()
-        else:
-            print(f"Warning: Training folder missing for product '{product_folder}'.")
-
-    # Note: Adjust the file extensions in the endsWith tuple based on the actual file formats of your images.
-
-image_path="Data\Product Classification"
-original_image = cv2.imread(image_path)
-dataset_path = "Data"
-readtrainimage(dataset_path)
-preprocessing(original_image)
 
 
 
